00de2b1ed00778106161cf40fea5be2dcf5c29dd
Rufusan <krysstian18@gmail.com>
2017-04-05 19:30:03 +0200

    Adding Services
    Add first template service

--- projekt_symfony/src/AppBundle/Controller/KrystianController.php
+++ projekt_symfony/src/AppBundle/Controller/KrystianController.php
@@ -10,16 +10,22 @@ namespace AppBundle\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class KrystianController
+class KrystianController extends Controller
 {
     /**
      * @Route("/krystian/{someName}")
      */
     public function showAction($someName)
     {
-        return new Response('Witaj '.$someName);
+        $templating = $this->container->get('templating');
+        $html = $templating->render('krystian/show.html.twig', [
+            'name' => $someName
+        ]);
+
+        return new Response($html);
     }
 }
 
